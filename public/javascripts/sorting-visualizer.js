cc6914979272c55bb9b62a43451672194cf09596
hasanTusher <hasan.tusher787@gmail.com>
2021-07-18 03:33:08 +0600

    sorting-visualizer.js single bar animation done

--- public/javascripts/sorting-visualizer.js
+++ public/javascripts/sorting-visualizer.js
@@ -14,9 +14,38 @@ function draw(size) {
    }
 }
 
+function exchangeAnimation() {
+   reqAnimFrame = window.requestAnimationFrame(exchangeAnimation);
+   if(x > 9*25){
+      return;
+   }
+   var canvas  = document.getElementById("myCanvas");
+   var context = canvas.getContext("2d");
+   context.clearRect(0, 0, 500, 500);
+   let isLastOne = false;
+   for(let i = 0; i < 10; i++) {
+      if(i == 0){
+         x+=5;
+         context.fillStyle = "#1a24cf";
+         context.fillRect(x, 100+(i*25), 25, arr[i]*25);
+      }else{
+         context.fillStyle = "#1a24cf";
+         context.fillRect(i*25, 100+(i*25), 25, arr[i]*25);
+      }
+   }
+   if(isLastOne){
+      return
+   }
+}
 document.addEventListener("DOMContentLoaded", function(event) {
    arr = [];
    let size = 10;
+   var rightMost = 9*25;
+   x = 0;
+   x2 = 9*25
    createArray(arr, size);
    draw(size);
+
+   exchangeAnimation(0, 9, 0, 9*25);
+
 });
