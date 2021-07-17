859b36eaf9ad0007f3a0493ccbba6a850bbdffd1
hasanTusher <hasan.tusher787@gmail.com>
2021-07-18 02:53:57 +0600

    initial draw finalized

--- public/javascripts/sorting-visualizer.js
+++ public/javascripts/sorting-visualizer.js
@@ -1,24 +1,22 @@
-function createArray(arr) {
-   for(let i = 10; i> 0; i--){
+function createArray(arr, size) {
+   for(let i = size; i > 0; i--){
       arr.push(i);
    }
 }
 
-function draw() {
+function draw(size) {
    var canvas  = document.getElementById("myCanvas");
    var context = canvas.getContext("2d");
    context.clearRect(0, 0, 500, 500);
-
-   let size = 10;
-   let x = 0;
    for(let i = 0; i < size; i++) {
-      context.fillStyle = "#ff00ff";
+      context.fillStyle = "#ac2c2c";
       context.fillRect(i*25, 100+(i*25), 25, arr[i]*25);
    }
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-   arr = [];s
-   createArray(arr);
-   draw();
+   arr = [];
+   let size = 10;
+   createArray(arr, size);
+   draw(size);
 });
